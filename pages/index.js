5f95e8bc942e430974fa5e112dd16232c4cfe12f
tuboihirokidesu <tuboi.hiroki@icloud.com>
2021-04-13 15:18:25 +0900

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const response = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: response.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
   return (
-    <div className="recipe-list">
-      Recipe List
+    <div className='recipe-list'>
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
     </div>
-  )
+  );
 }
